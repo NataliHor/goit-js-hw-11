6de13dca1a4c5595d9f66b0b445aaea75a22cfbb
Nataliia Horokhovatska <nataliia.horokhovatska@gmail.com>
2024-07-10 11:38:31 +0300

    hw11

--- src/render-functions.js
+++ src/render-functions.js
@@ -1,7 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const lightbox = new SimpleLightbox('.gallery-item a', {
+let lightbox = new SimpleLightbox('.gallery-item a', {
   captionsData: 'alt',
   captionDelay: 250,
 }).refresh();
