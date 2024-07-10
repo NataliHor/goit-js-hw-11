8f3cdce94c40662e803770180713f7931eecbcf1
Nataliia Horokhovatska <nataliia.horokhovatska@gmail.com>
2024-07-10 12:55:59 +0300

    hw11

--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -2,7 +2,7 @@ export function getPictures(query) {
   const API_KEY = '44809751-3b2ccf197f9e8e95d68b14f11';
 
   return fetch(
-    `https://pixabay.com/api?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+    `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
   ).then(res => {
     if (!res.ok) {
       throw new Error(res.status);
